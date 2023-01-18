57c566feecc3efa44ed42cb66074aa01a4ff05fb
uomaep <uomaep@gmail.com>
2023-01-18 21:01:29 +0900

    즐겨찾기 탭 추가

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/favorites_widget.dart';
 import '../widgets/searchBar_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,24 +14,24 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: const Color(0xFF1c1c1f),
-          title: const Text(
-            "OP.GG",
-            style: TextStyle(
-              color: Color(0xFF5383e8),
-              fontSize: 30,
-              fontWeight: FontWeight.w800,
-            ),
+          title: Row(
+            children: const [
+              Text(
+                "OP.GG",
+                style: TextStyle(
+                  color: Color(0xFF5383e8),
+                  fontSize: 30,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ],
           ),
         ),
         body: Column(
           children: [
             const SearchBar(),
             Image.asset('assets/images/2022_awards.png'),
-            Container(
-              height: 40,
-              color: Colors.blue,
-              child: Text("World!"),
-            ),
+            Favorites(),
           ],
         ),
       ),
---
+++ lib/widgets/favorites_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Favorites extends StatelessWidget {
+  const Favorites({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      color: const Color(0xFF1c1c1f),
+      width: double.infinity,
+      height: 150,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "즐겨찾기한 소환사",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.0,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 25),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow,
+                    ),
+                    Text(
+                      "즐겨찾기한 소환사가 없습니다.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  "소환사를 검색한 후 등록해주세요!",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
